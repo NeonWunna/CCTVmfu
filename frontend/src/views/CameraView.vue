9c3efd2df558b133e3be3a528b902dd4dd820660
TunDOTio <tuntun.pam3164@gmail.com>
2026-02-23 15:53:53 +0700

    Use Universal stream.html player for graceful MSE fallback

--- frontend/src/views/CameraView.vue
+++ frontend/src/views/CameraView.vue
@@ -328,7 +328,7 @@ onBeforeUnmount(() => {
 
             <iframe
               v-else
-              :src="`/stream/webrtc.html?src=${encodeURIComponent(cameraData.rtspUrl)}`"
+              :src="`/stream/stream.html?src=${encodeURIComponent(cameraData.rtspUrl)}`"
               class="video-stream"
               title="Live Camera Feed"
               frameborder="0"
