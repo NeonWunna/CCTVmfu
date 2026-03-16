fbffd5771c97645ad22899c4b7dfdd28040ac31c
TunDOTio <tuntun.pam3164@gmail.com>
2026-03-16 12:48:35 +0700

    q

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.middleware.sessions import SessionMiddleware
+from starlette.middleware.sessions import SessionMiddleware
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 
